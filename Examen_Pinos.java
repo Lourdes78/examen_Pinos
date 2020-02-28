1dc395b759bace3517b187f87c8bdb0d68ea1484
Loucel1978 <loucelpinos@gmail.com>
2020-02-28 09:33:15 +0100

    Examen Pinos

---
+++ Examen_Pinos.java
@@ -0,0 +1,16 @@
+
+public class Examen_Pinos {
+	public static void main (String[] args) {
+		   printOne();
+		   printOne();
+		   printTwo();
+	}
+	public static void printOne() {
+		System.out.println("Hello world");
+	}
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+
+}
